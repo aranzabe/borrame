635c6a15f43491ac5c9e415a5438a1ef8f67eb61
Fernando Aranzabe <faranzabe@gmail.com>
2022-07-06 08:28:26 +0200

    Commit main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val n:Int = 4
+    println("Valor:  ${n}")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
 }
